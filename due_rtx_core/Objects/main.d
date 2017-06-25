7ff464c66acc69db47f6072b5399c39cc28e3879
Ebin Baby <ebinishere@gmail.com>
2017-06-25 01:45:40 -0500

    Clean commit

--- due_rtx_core/Objects/main.d
+++
@@ -1,5 +0,0 @@
-.\objects\main.o: src\main.c
-.\objects\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.0.1\CMSIS\RTOS\RTX\INC\cmsis_os.h
-.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
-.\objects\main.o: src\task.h
--- due_rtx_core/Objects/startup_sam3xa.d
+++
@@ -1,1 +0,0 @@
-.\objects\startup_sam3xa.o: RTE\Device\ATSAM3X8E\startup_SAM3XA.s
